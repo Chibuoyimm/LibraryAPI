a2c70a411c57e5a7a650671d5d001a90ac5b17fb
Chibuoyim <onuigwechibuoyim@gmail.com>
2022-09-11 22:37:54 +0100

    Added routing functionality

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,5 +6,9 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router){
-	router.HandleFunc("/book/", cont)
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
