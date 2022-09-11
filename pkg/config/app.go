fef30d1822076170b310248f904f17b9f997ac23
Chibuoyim <onuigwechibuoyim@gmail.com>
2022-09-11 23:02:50 +0100

    Created a connection with the MySQL Database

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"  // the documentation said to do that
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect(){
+	d, err := gorm.Open("mysql", "sql6518910:MBjRyMUuSd@/sql6518911?charset=utf8&parseTime=True&loc=Local") // setting up mysql connection "username:password@/databasename"
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
