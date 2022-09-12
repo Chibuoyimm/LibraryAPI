a61e03400f7322e219ec53584e5f8bd9b7c788ea
Chibuoyim <onuigwechibuoyim@gmail.com>
2022-09-12 19:44:27 +0100

    Added some more code in models

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"gihub.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/chibuoyimm/go-bookstore/pkg/utils"
+	"github.com/chibuoyimm/go-bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request){
+	new 
+}
